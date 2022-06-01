d1237a57c32a0f186f27d1090fc15b1ab6c485f3
forhad-eng <forhadu.cse@gmail.com>
2022-06-01 17:45:15 +0600

    one possible issue fixed

--- src/Pages/Login/RequireAdmin.js
+++ src/Pages/Login/RequireAdmin.js
@@ -1,7 +1,7 @@
 import { signOut } from 'firebase/auth'
 import React from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useNavigate } from 'react-router-dom'
+import { Navigate, useLocation, useNavigate } from 'react-router-dom'
 import auth from '../../Firebase/firebase.init'
 import useAdmin from '../../hooks/useAdmin'
 import LoadingSpinner from '../Shared/LoadingSpinner'
@@ -10,9 +10,14 @@ const RequireAdmin = ({ children }) => {
     const [user, loading] = useAuthState(auth)
     const [admin, adminLoading] = useAdmin(user)
     const navigate = useNavigate()
+    const location = useLocation()
 
     if (loading || adminLoading) {
         return <LoadingSpinner />
+    }
+
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace />
     }
 
     if (!admin) {
